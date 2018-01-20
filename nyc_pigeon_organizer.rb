7b1273717145e1ad6ef5684eb3ff4b57dc33e8f0
johnvoyage <jmyork762@gmail.com>
2018-01-19 19:59:16 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |main_stat, hash_one|
+    hash_one.each do |sub_stat, name_arr|
+      name_arr.each do |index|
+        if !pigeon_list[index]
+          pigeon_list[index] = {
+            main_stat => [sub_stat.to_s]
+          }
+        elsif main_stat == :color
+          pigeon_list[index][:color] << sub_stat.to_s
+        else
+          pigeon_list[index][main_stat] = [sub_stat.to_s]
+        end
+      end
+    end
+  end
+  pigeon_list
 end
